f6d5201dc15f7422384b4e4fabe17ccfae0751d3
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-09-23 18:03:42 +0600

    Make Donate And History Button Clickable.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,25 @@
-console.log('Sheikh');
+addToHistory();
+
+const donateBtn = document.getElementById('donate-btn');
+
+donateBtn.addEventListener('click', () => {
+    donateBtn.classList.add('border-lightGreen', 'bg-lightGreen');
+    donateBtn.classList.remove('border-slate-300', 'bg-transparent');
+    historyBtn.classList.remove('border-lightGreen', 'bg-lightGreen');
+    historyBtn.classList.add('border-slate-300', 'bg-transparent');
+
+    document.getElementById('donation-main').classList.remove('hidden');
+    document.getElementById('history-main').classList.add('hidden');
+})
+
+
+const historyBtn = document.getElementById('history-btn');
+historyBtn.addEventListener('click', () => {
+    historyBtn.classList.add('border-lightGreen', 'bg-lightGreen');
+    historyBtn.classList.remove('border-slate-300', 'bg-transparent');
+    donateBtn.classList.remove('border-lightGreen', 'bg-lightGreen');
+    donateBtn.classList.add('border-slate-300', 'bg-transparent');
+
+    document.getElementById('history-main').classList.remove('hidden');
+    document.getElementById('donation-main').classList.add('hidden');
+})
