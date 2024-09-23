293ee49f42fdbc5c61efddc1060cb7e52d525bab
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-09-23 17:43:13 +0600

    Make Add to History Utility Function 2 (Add JS File);

--- JS/utility.js
+++ JS/utility.js
@@ -1,1 +1,18 @@
-console.log('Antor');
+// Add To History Function ;
+function addToHistory(amount, title) {
+    const historyDev = document.getElementById('history-div');
+
+    const div = `
+    <div class="border-2 border-green-400 rounded-lg p-4 mb-5">
+        <h1 class="text-xl mb-2">${amount} Taka is Donated ${title}</h1>
+        <p class="bg-slate-100 p-2 font-semibold rounded-lg">
+            Date : ${new Date}
+        </p>
+    </div>
+    `
+    historyDev.innerHTML += div;
+}
+
+
+
+
